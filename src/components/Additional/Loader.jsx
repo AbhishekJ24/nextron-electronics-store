89987d852e702d34debb0d123d5dd87c3c4b0f3f
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-06-12 22:48:54 +0530

    Loader fixed

--- src/components/Additional/Loader.jsx
+++ src/components/Additional/Loader.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="loader rounded-full ease-linear h-10 w-10"></div>
+    <div className="loader">
     </div>
   );
 }
