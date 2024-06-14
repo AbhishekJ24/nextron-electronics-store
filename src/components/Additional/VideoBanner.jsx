7a2545e98ff2362c09c427f2d80d24b505093091
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-06-14 16:49:07 +0530

    Added a sample video banner

--- src/components/Additional/VideoBanner.jsx
+++ src/components/Additional/VideoBanner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function VideoBanner({ video_url }) {
-  return <video autoPlay loop muted className="min-w-full" src={video_url}></video>;
+  return <video autoPlay loop className="min-w-full" src={video_url}></video>;
 }
 
 export default VideoBanner;
--- src/components/pages/Home/page.jsx
+++ src/components/pages/Home/page.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import VideoBanner from "../../Additional/VideoBanner";
 
 function Home() {
   return (
     <div>
-
+      <div className="border-t-4 border-b-4">
+      <VideoBanner video_url="nextron-advertisement.mp4" />
+      </div>
     </div>
   );
 }
