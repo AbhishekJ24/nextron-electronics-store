002177f485fa0a618a5f559fd0d05bcefb218df1
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-06-20 12:19:53 +0530

    Footer completed with responsiveness

---
+++ src/components/Additional/NewsletterForm.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+    email: "",
+};
+
+const validationSchema = Yup.object({
+    email: Yup.string()
+        .required("This field is required")
+
+});
+
+const onSubmit = (values) => {
+    console.log(values);
+};
+
+function NewsletterForm() {
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <Form>
+                <div className="text-slate-200 font-medium text-xl mb-2">Subscribe to our newsletter</div>
+                <div className="">
+                    <Field name="email">
+                        {(props) => {
+                            const { field, form, meta } = props;
+                            return (
+                                <>
+                                    <input
+                                        className="px-3 py-1 mb-2 rounded-lg outline-none"
+                                        type="text"
+                                        id="email"
+                                        placeholder="Email Address"
+                                        {...field}
+                                    />
+                                </>
+                            );
+                        }}
+                    </Field>
+                </div>
+                <div className="">
+                    <button className="searchQueryButton transition-all hover:ease-in rounded-md px-4 py-[0.125rem]" type="submit">
+                        Subscribe
+                    </button>
+                </div>
+            </Form>
+        </Formik>
+    )
+}
+
+export default NewsletterForm
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,104 +2,96 @@ import React from "react";
 import { FaFacebook } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa6";
+import NewsletterForm from "./Additional/NewsletterForm";
 
 function Footer() {
   return (
-    <div className="footer standard-responsive py-10 sm:flex sm:gap-56 lg:gap-96">
-      <div className="mb-10 mx-10 sm:mx-px lg:w-1/4">
-        <div className="text-white text-3xl font-bold mb-2">
+    <div className="footer standard-responsive py-14 flex flex-wrap gap-10 md:gap-0 md:justify-between">
+
+      <div className="sm:w-1/4 mb-5">
+        <div className="text-white font-bold text-xl mb-2">
           Our store
         </div>
-        <h2 className="mb-5 lg:text-xl">
+        <h2>
           Nextron Electronics Store, based in NYC, offers the latest gadgets and
           devices, from smartphones and laptops to gaming gear and home
-          entertainment systems. We prioritize quality, competitive pricing, and
-          exceptional customer service. Explore the future of technology with us
-          at Nextron Electronics Store.
+          entertainment systems.
         </h2>
 
-        <div className="flex gap-5">
+        <div className="flex gap-5 mt-3">
           <a target="_blank" href="https://www.facebook.com/">
-            <FaFacebook
-              className="hover:scale-105 hover:ease-out transition-all"
-              size={25}
-            />
+            <FaFacebook size={25} />
           </a>
 
           <a target="_blank" href="https://www.instagram.com/">
-            <FaInstagram
-              className="hover:scale-105 hover:ease-out transition-all"
-              size={28}
-            />
+            <FaInstagram size={28} />
           </a>
 
           <a target="_blank" href="https://www.youtube.com/">
-            <FaYoutube
-              className="hover:scale-105 hover:ease-out transition-all"
-              size={30}
-            />
+            <FaYoutube size={30} />
           </a>
         </div>
       </div>
 
-      <div className="mx-10 sm:mx-px lg:w-3/4 flex flex-wrap gap-12">
-        <ul>
-          <li className="text-white mb-3 text-3xl">Products</li>
-          <li>
-            <a className="lg:text-xl" href="/">Televisions and Accessories</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Home Appliances</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Phone and Wearables</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Audio and Video</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Cameras and Accessories</a>
-          </li>
-        </ul>
+      <ul className="mb-5">
+        <li className="text-white font-semibold">Products</li>
+        <li>
+          <a href="/">Televisions and Accessories</a>
+        </li>
+        <li>
+          <a href="/">Home Appliances</a>
+        </li>
+        <li>
+          <a href="/">Phone and Wearables</a>
+        </li>
+        <li>
+          <a href="/">Audio and Video</a>
+        </li>
+        <li>
+          <a href="/">Cameras and Accessories</a>
+        </li>
+      </ul>
 
-        <ul className="">
-          <li className="text-white mb-3 text-3xl">Company</li>
-          <li>
-            <a className="lg:text-xl" href="/">About Us</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Careers</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Buying Guide</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">FAQs</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Privacy Policy</a>
-          </li>
-        </ul>
+      <ul className="mb-5">
+        <li className="text-white font-semibold">Company</li>
+        <li>
+          <a href="/">About Us</a>
+        </li>
+        <li>
+          <a href="/">Careers</a>
+        </li>
+        <li>
+          <a href="/">Buying Guide</a>
+        </li>
+        <li>
+          <a href="/">FAQs</a>
+        </li>
+        <li>
+          <a href="/">Privacy Policy</a>
+        </li>
+      </ul>
 
-        <ul className="">
-          <li className="text-white mb-3 text-3xl">Let Us Help You</li>
-          <li>
-            <a className="lg:text-xl" href="/">Your Account</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Returns Center</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">100% Purchase Protection</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Nextron App Download</a>
-          </li>
-          <li>
-            <a className="lg:text-xl" href="/">Help</a>
-          </li>
-        </ul>
-      </div>
+      <ul className="mb-5">
+        <li className="text-white font-semibold">Let Us Help You</li>
+        <li>
+          <a href="/">Your Account</a>
+        </li>
+        <li>
+          <a href="/">Returns Center</a>
+        </li>
+        <li>
+          <a href="/">100% Purchase Protection</a>
+        </li>
+        <li>
+          <a href="/">Nextron App Download</a>
+        </li>
+        <li>
+          <a href="/">Help</a>
+        </li>
+      </ul>
+      <ul className="mb-5">
+        <NewsletterForm/>
+      </ul>
     </div>
   );
 }
