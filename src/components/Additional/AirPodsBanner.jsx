0ae54d612c64a9c06a4e6fcd22950bc92a520a38
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-06-20 11:20:49 +0530

    Home Page Finished

--- src/components/Additional/AirPodsBanner.jsx
+++ src/components/Additional/AirPodsBanner.jsx
@@ -29,17 +29,17 @@ function AirPodsBanner() {
             <div className="pt-1 sm:hidden text-center">
                 <span className='block title-airpods text-5xl font-bold drop-shadow-2xl'>AirPods Pro</span>
                 <span className='block title-2-airpods text-xl font-semibold drop-shadow-2xl'>2nd Generation</span>
-                <button className='inter block m-auto mt-2 py-2 px-8 rounded-xl text-base bg-slate-800 hover:bg-slate-700 transition-all hover:ease-in text-slate-50 font-semibold'>Buy $249</button>
+                <button className='inter block m-auto mt-2 py-2 px-8 rounded-xl text-sm bg-slate-800 hover:bg-slate-700 transition-all hover:ease-in text-slate-50 font-semibold'>Buy $249</button>
             </div>
-            <div className="hidden sm:block ml-36 pt-36">
+            <div className="hidden sm:block ml-44 pt-36">
                 <span className="title-airpods text-[2.5rem] sm:text-[3rem] md:text-[3.5rem] lg:text-[5rem] xl:text-[7rem] block font-bold drop-shadow-2xl">AirPods Pro</span>
                 <span className="title-2-airpods text-[0.8rem] sm:text-[1rem] md:text-[1.25rem] lg:text-[1.5rem] xl:text-[2rem] block font-semibold mb-4 drop-shadow-2xl">2nd Generation</span>
-                <button className="inter block rounded-xl bg-slate-800 hover:bg-slate-700 transition-all hover:ease-in text-slate-50 px-8 py-2 text-lg font-semibold drop-shadow-2xl">
+                <button className="block rounded-xl bg-slate-800 hover:bg-slate-700 transition-all hover:ease-in text-slate-50 px-6 py-2 text-base font-semibold drop-shadow-2xl">
                     Buy $249
                 </button>
             </div>
             <img
-                className="absolute hidden h-[16rem] sm:block sm:h-[22rem] md:h-[20rem] lg:h-[28rem] xl:h-[33rem] right-10 sm:top-16 md:top-20 lg:top-14 xl:top-12 drop-shadow-2xl"
+                className="absolute hidden h-[16rem] sm:block sm:h-[22rem] md:h-[20rem] lg:h-[28rem] xl:h-[33rem] right-20 sm:top-16 md:top-20 lg:top-14 xl:top-12 drop-shadow-2xl hover:scale-105 transition-all hover:ease-out hover:opacity-75"
                 src={airpods[airIndex].src}
                 alt="not-found"
             />
