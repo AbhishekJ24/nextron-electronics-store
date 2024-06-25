cfd42b63d0002e228bd15364b9fe14648a1bc477
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-06-25 14:24:24 +0530

    contact us page adjusted

--- src/components/Additional/AddressMap.jsx
+++ src/components/Additional/AddressMap.jsx
@@ -5,7 +5,7 @@ const AddressMap = ({ address_url }) => {
     <div style={{ position: 'relative', paddingBottom: '50%', height: 0, overflow: 'hidden' }}>
       <iframe
         src={address_url}
-        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '80%' }}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
