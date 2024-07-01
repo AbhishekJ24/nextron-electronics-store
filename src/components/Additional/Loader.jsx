2710400199455c78ceed9b406aa70a9303f6af56
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-07-02 01:55:10 +0530

    .

--- src/components/Additional/Loader.jsx
+++ src/components/Additional/Loader.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="loader">
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="w-14 h-14 loader rounded-full animate-spin"></div>
     </div>
   );
 }
