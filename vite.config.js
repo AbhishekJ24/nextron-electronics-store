a7a942ceb5f58a3f8959b6af19f767040838a12e
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-07-02 18:19:05 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    base: env.VITE_ROUTER_BASE_URL || '/',
+    plugins: [react()],
+    define: {
+      'process.env': {
+        VITE_KEY_ID: env.VITE_KEY_ID,
+        VITE_KEY_SECRET: env.VITE_KEY_SECRET,
+      },
+    },
+  });
+};
