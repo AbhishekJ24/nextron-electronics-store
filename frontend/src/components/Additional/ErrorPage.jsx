9e85033e1fd6672f3fb13efb2207be57665e67c5
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-07-05 17:46:49 +0530

    ErrorPage design refined

--- frontend/src/components/Additional/ErrorPage.jsx
+++ frontend/src/components/Additional/ErrorPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function ErrorPage() {
   return (
-    <div className="flex items-center justify-center">
-      <p className="text-xl md:text-2xl lg:text-3xl font-bold text-slate-900">
+    <div className="flex items-center justify-center w-screen h-[31rem] bg-slate-100">
+      <p className="text-xl md:text-2xl lg:text-3xl font-bold text-slate-900 raleway">
         Sorry for the issue, we're still under construction. Visit us again!
       </p>
     </div>
