e89d15a363b00c542f836d5d490695e87b47074e
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-07-10 11:52:24 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
 import apiRoutes from './routes/api.js';
 
 const app = express();
 app.use(cors());
+app.set('views', path.join(path.dirname(fileURLToPath(import.meta.url)), 'views'));
 app.set('view engine', 'ejs');
 
 await mongoose.connect('mongodb://localhost:27017/nextron-electronics-store')
@@ -12,7 +15,7 @@ await mongoose.connect('mongodb://localhost:27017/nextron-electronics-store')
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.render('home.ejs',{});
+    res.render('home.ejs');
 });
 
 app.use('/api', apiRoutes);
